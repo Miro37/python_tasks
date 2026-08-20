abd5d0dbdd8f022d50708b0abc33de1c8836b257
Miro37 <mmujunen21@gmail.com>
2026-08-20 13:26:36 +0300

    tunti tehtävät

---
+++ Moduuli1/tunti_teht/laskuri.py
@@ -0,0 +1,4 @@
+paiva= int(input("Kuinka monta päivää: "))
+sekunnit = paiva * 24 * 60 * 60
+print(f"sekuntien määrä: {sekunnit}")
+
---
+++ Moduuli1/tunti_teht/muunnin.py
@@ -0,0 +1,4 @@
+grammat=int(input("Kuinka monta grammaa: "))
+vastaus_kilo=grammat//1000
+vastaus_grammat=grammat % 1000
+print(f"Määrä kiloina ja grammoina: {vastaus_kilo}kg ja {vastaus_grammat}g")
