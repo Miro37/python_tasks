4a42034e12a3cb6d55488c7a0dc442cce60aeb5f
Miro37 <mmujunen21@gmail.com>
2026-08-20 10:05:16 +0300

    tein tehtävän 2.1 ja siirsin oikeaan fileen

---
+++ Moduuli1/tunti_teht/tarina.py
@@ -0,0 +1,6 @@
+nimi=input("Anna nimesi:")
+aikakausi=input("Anna aikakausi:")
+adjektiivi=input("Anna jokin adjektiivi")
+identiteetti=input("Kuka olet")
+
+print(f"Olipa kerran {nimi}, joka oli {adjektiivi} {identiteetti}. {nimi} haastoi aina riitaa naapurustossa ja teki ilkeyksiä. Eräänä päivänä {nimi} päätti lopettaa riidan haastamisen ja päätti muuttaa pois ")
