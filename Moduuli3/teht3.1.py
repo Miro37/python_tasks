d4743d68be967518c37700063b2339a045654b46
Miro37 <mmujunen21@gmail.com>
2026-08-26 15:15:07 +0300

    korjasin koodia

--- Moduuli3/teht3.1.py
+++ Moduuli3/teht3.1.py
@@ -1,5 +1,7 @@
 kuha=float(input("Anna kuhan pituus: "))
 if kuha < 37:
-    print("Kuha on alamittainen laske se takaisin veteen")
+    alamitta=37-kuha
+    alamitta_vastaus=37-alamitta
+    print(f"Kuhasta puuttuu {alamitta}cm laske se takaisin veteen")
 else:      
-    print(kuha,"cm")
+    print("kuha on hyvän mittainen")
