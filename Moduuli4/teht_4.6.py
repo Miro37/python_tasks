5c135c627e56884b8b6b58e7f4e6f326c4883704
Miro37 <mmujunen21@gmail.com>
2026-08-27 13:10:01 +0300

    4.6 (kesken)

---
+++ Moduuli4/teht_4.6.py
@@ -0,0 +1,14 @@
+#import math
+#import random
+#piste_maara=int(input("Anna pisteiden määrä: "))
+#ympyra_piste=0
+
+#for i in range(piste_maara):
+    #x=(-1, 1)
+    #y=(-1, 1)
+    #if x*2*2 + y*2*2 <1:
+       # ympyra_piste+=1
+
+#pi=4*ympyra_piste/piste_maara
+
+#print(f"Pi:n likiarvo on {pi}")
